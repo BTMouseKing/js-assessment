86715405318387b1ce6487109dccc2c108e22e53
ZHEMIN ZHANG <zhemin.jimmy.zhang@gmail.com>
2016-09-15 16:33:01 -0400

    Tried a new way.

--- app/async.js
+++ app/async.js
@@ -3,25 +3,24 @@ exports = typeof window === 'undefined' ? global : window;
 exports.asyncAnswers = {
   async: function(value) {
       return new Promise(function(resolve, reject) {
-            resolve(value);
+          resolve(value);
       });
   },
 
   manipulateRemoteData: function(url) {
-      var defer = $.Deferred();
-      $.ajax({
-        dataType: "json",
-        url: url,
-        success: function(data) {
-            var ret = [];
-            data.people.forEach( function (person) {
-                ret.push(person.name);
-            });
-            ret.sort();
-            defer.resolve(ret);
-        }
+      return new Promise(function(resolve, reject) {
+          $.ajax({
+            dataType: "json",
+            url: url,
+            success: function(data) {
+                var ret = [];
+                data.people.forEach( function (person) {
+                    ret.push(person.name);
+                });
+                ret.sort();
+                resolve(ret);
+            }
+          });
       });
-
-      return defer.promise();
   }
 };
