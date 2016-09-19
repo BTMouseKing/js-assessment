2bd687e86a7ed1e1d73cd9f74a21cf0dfbffc43c
ZHEMIN ZHANG <zhemin.jimmy.zhang@gmail.com>
2016-09-19 12:06:49 -0400

    Complete the test for Recursion.

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-
+      var files = [];
+      for (var property in data) {
+          if (property == "files") {
+              var val = data[property];
+              if (val instanceof Array) {
+                  for (var i = 0; i < val.length; i++) {
+                      if (val[i] instanceof Object) {
+                          files = files.concat(this.listFiles(val[i], dirName));
+                      } else {
+                          if (dirName !== undefined) {
+                              if (val[i].indexOf(dirName) !== -1) {
+                                    files.push(val[i]);
+                              }
+                          } else {
+                              files.push(val[i]);
+                          }
+                      }
+                  }
+              }
+          }
+      }
+      return files;
   },
 
   permute: function(arr) {
